650eed4a048d775dc4d64b38570a84adbcf7a91f
Piotr Nastaj <piotr@nastaj.net>
2018-09-26 14:39:29 +0200

    Update script.js with exercise 1-5 and my non-working creation

--- script-compiled.js
+++ script-compiled.js
@@ -1,6 +1,78 @@
+// test code from previous submodule
+
 'use strict';
 
 var sayHello = function sayHello() {
-  return alert('Hello world!');
+	console.log('Hello world - test!');
 };
 sayHello();
+
+// exercise 1 - connecting strings
+var part1_1 = 'Hello';
+var part1_2 = 'World';
+var part1_3 = 1;
+var part1_all = part1_1 + ' ' + part1_2 + ' ' + part1_3;
+
+//exercise 2 - missing arguments w/ arrow functions
+var multiply = function multiply() {
+	var arg1 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
+	var arg2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+	return arg1 * arg2;
+};
+
+//exercise 3 - rest params /w arrow functions
+var average = function average() {
+	for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+		args[_key] = arguments[_key];
+	}
+
+	return args.reduce(function (prev, curr) {
+		return prev + curr;
+	}) / args.length;
+};
+
+//exercise 4 - passing array to exercise 3
+var grades = [8, 5, 5, 5, 4, 3, 3, 2, 1];
+var passedGrades = average.apply(undefined, grades);
+
+//exercise 5 - destructurizing array
+var veryStrangeArray = [1, 4, 'Iwona', false, 'Nowak'];
+var firstName = veryStrangeArray[2],
+    lastName = veryStrangeArray[4];
+
+var exercise5check = firstName === 'Iwona' && lastName === 'Nowak' ? 5 : 'did not work';
+
+// testing 1
+console.log(part1_all + ' OK!');
+// testing 2
+console.log('Hello World ' + multiply(2) + ' OK!');
+// testing 3
+console.log('Hello World ' + average(1, 3, 5) + ' OK!');
+// testing 4
+console.log('Hello World ' + passedGrades + ' OK!');
+// testing 5
+console.log('Hello World ' + exercise5check + ' OK!');
+
+// aaaaand I got lost trying to compare a [1,2,3,4,5] array to another [1,2,3,4,5] array... :/
+
+var allTestResults = [part1_3, multiply(2), average(1, 3, 5), passedGrades, exercise5check];
+var mapAllTestResults = allTestResults.map(function (item, index) {
+	return index + 1;
+});
+
+var checkAll = function checkAll() {
+	for (var i = 0; i < allTestResults.length; i++) {
+		if (allTestResults[i] !== mapAllTestResults[i]) {
+			return false;
+		}
+		return true;
+	}
+};
+
+var finalAnswer = checkAll() === true ? "ES6 is great !" : "I am stupid";
+
+console.log('allTestResulsts:', allTestResults);
+console.log('mapAllTestResults: ', mapAllTestResults);
+
+console.log('checkAll(): ' + checkAll());
+console.log('finalAnswer: ' + finalAnswer);
--- script.js
+++ script.js
@@ -1,2 +1,69 @@
-const sayHello = () => alert('Hello world!');
+// test code from previous submodule
+
+'use strict';
+
+var sayHello = function sayHello() {
+	console.log('Hello world - test!');
+};
 sayHello();
+
+// exercise 1 - connecting strings
+const part1_1 = 'Hello';
+const part1_2 = 'World';
+const part1_3 = 1;
+const part1_all = `${part1_1} ${part1_2} ${part1_3}`;
+
+//exercise 2 - missing arguments w/ arrow functions
+const multiply = (arg1 = 1 , arg2 = 1) => (arg1 * arg2);
+
+//exercise 3 - rest params /w arrow functions
+const average = (...args) => (args.reduce((prev, curr) => prev + curr) / args.length);
+
+//exercise 4 - passing array to exercise 3
+const grades = [8, 5, 5, 5, 4, 3, 3, 2, 1];
+const passedGrades = average(...grades);
+
+//exercise 5 - destructurizing array
+const veryStrangeArray = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstName , , lastName] = veryStrangeArray;
+const exercise5check = (firstName === 'Iwona' && lastName ==='Nowak') ? 5 : 'did not work';
+
+
+
+
+// testing 1
+console.log(`${part1_all} OK!`);
+// testing 2
+console.log(`Hello World ${multiply(2)} OK!`);
+// testing 3
+console.log(`Hello World ${average(1,3,5)} OK!`);
+// testing 4
+console.log(`Hello World ${passedGrades} OK!`);
+// testing 5
+console.log(`Hello World ${exercise5check} OK!`);
+
+
+
+
+
+// aaaaand I got lost trying to compare a [1,2,3,4,5] array to another [1,2,3,4,5] array... :/
+
+let allTestResults = [part1_3 , multiply(2) , average(1,3,5) , passedGrades , exercise5check];
+let mapAllTestResults = allTestResults.map((item,index) => (index + 1));
+
+let checkAll = function (){
+	for (let i=0 ; i < allTestResults.length ; i++) {
+		if (allTestResults[i] !== mapAllTestResults[i]){
+			return false;
+		}
+		return true;
+	}
+}
+
+let finalAnswer = checkAll() === true  ? "ES6 is great !" : "I am stupid";
+
+console.log('allTestResulsts:', allTestResults);
+console.log('mapAllTestResults: ', mapAllTestResults);
+
+console.log(`checkAll(): ${checkAll()}`);
+console.log(`finalAnswer: ${finalAnswer}`);
